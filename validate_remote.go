e2187080c1ea72c8c97dff13809dc47a9472fc17
Piotr Mozdzynski <piotr.mozdzynski@williamhill.com>
2025-02-27 10:36:31 +0100

    Output errors when the cert/chain is broken

--- validate_remote.go
+++ validate_remote.go
@@ -22,16 +22,21 @@ func getCertFromRemoteServer(hostname string) ([]*x509.Certificate, error) {
 }
 
 // Verifies the certificate chain presented by the remote server
-func verifyChain(certs []*x509.Certificate) {
+func verifyChain(certs []*x509.Certificate) bool {
+	isValid := true
+
 	for i := len(certs) - 1; i > 0; i-- {
 		expiry := certs[i].NotAfter
 		if err := certs[i-1].CheckSignatureFrom(certs[i]); err != nil {
 			fmt.Printf("Certificate at position %d has abnormality. It's not signed by certificate at position %d. Error: %v - Expires: %v\n", i+1, i, err, expiry)
+			isValid = false
 		} else {
 			fmt.Printf("Certificate at position %d is normal. It's signed by certificate at position %d. Expires: %v\n", i+1, i, expiry)
 		}
 	}
 	fmt.Printf("Certificate at position 1 is the Root Certificate. Expires: %v\n", certs[0].NotAfter)
+
+	return isValid
 }
 
 // Validate a server certificate against its own chain
@@ -85,15 +90,15 @@ func main() {
 
 	// Verifying the server's certificate chain
 	fmt.Println("\nVerifying remote server certificate chain:")
-	verifyChain(remoteCerts)
+	chainValid := verifyChain(remoteCerts)
 
-	// Validate the first (leaf) certificate against the rest of the chain
-	if len(remoteCerts) > 0 {
+	// Validate the first (leaf) certificate against the rest of the chain only if the chain is valid
+	if chainValid {
 		fmt.Println("\nValidating remote server certificate against its own chain:")
 		if err := validateCertAgainstChain(remoteCerts[0], remoteCerts); err != nil {
 			fmt.Printf("Remote server certificate validation against chain failed: %v\n", err)
 		}
 	} else {
-		fmt.Println("\nNo remote certificates found to validate.")
+		fmt.Println("\nERROR !!! Skipping certificate validation due to errors in the certificate chain. ERROR!!!")
 	}
 }
